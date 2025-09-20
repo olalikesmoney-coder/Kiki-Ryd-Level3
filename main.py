34db062f00d46e11039aa5ad5e41d24a38041ead
olalikesmoney-coder <ola.likes.money@gmail.com>
2025-09-20 19:16:19 +0000

    add.more words

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import pygame
-from pygame ipmort draw, display
+from pygame Import draw, display
 
 #set up pygame window
-pygame.init(0)
+pygame.init()
 window=display.set_mode((600,600))
+pratcising with arguments and properties
+
+draw.rect(window, "white", house)
+draw.rect(window, "orangered", door)
+draw.rect(window, "blue", window1)
+draw.rect(window, "blue", window2)
+draw.rect(window, "dark grey", doorKnob)
+draw.rect(window, "grey", window1_1)
+draw.rect(window, "grey", window1_2)
+draw.rect(window, "brown", roof)
+#  A sample of while loops to Update the display
+while True:
+  display.update()
+
